d7196a87a886bef8f9c7ccc59be47da4fd72c9c2
FilipAntolic9 <fantolic@gmail.com>
2024-10-28 06:30:37 +0100

    T25

--- routes/ticket-details.js
+++ routes/ticket-details.js
@@ -2,6 +2,22 @@ var express = require('express');
 var router = express.Router();
 const db = require('../db');
 
+// DD/MM/YYYY HH:mm
+function formatDate(dateString) {
+    const options = { day: '2-digit', month: '2-digit', year: 'numeric', hour: '2-digit', minute: '2-digit', hour12: false };
+    return new Date(dateString).toLocaleDateString('hr-HR', options).replace(',', '');
+}
+
+function showErrorPage(res, message, status, errorStack = '') {
+    res.status(status).render('error', {
+        title: 'Error',
+        message: message,
+        status: status,
+        errorStack: errorStack
+    });
+}
+
+
 router.get('/:ticketId', async function (req, res, next) {
     const ticketId = req.params.ticketId;
 
@@ -9,10 +25,7 @@ router.get('/:ticketId', async function (req, res, next) {
         const result = await db.query("SELECT * FROM qrcodes WHERE id = $1;", [ticketId]);
 
         if (result.rows.length === 0) {
-            return res.status(404).json({
-                message: "Ulaznica nije pronađena.",
-                status: 404
-            });
+            return showErrorPage(res, "Ulaznica nije pronađena.", 404);
         }
 
         const ticket = result.rows[0];
@@ -21,12 +34,15 @@ router.get('/:ticketId', async function (req, res, next) {
         res.render('ticket-details', {
             title: 'Podatci o ulaznici',
             ticket: ticket,
-            userName: userName
+            userName: userName,
+            formattedDate: formatDate(ticket.datecreated)
         });
 
     } catch (err) {
+        // console.error(err);
+        // res.status(500).send('Stranica je trenutno nedostupna.');
         console.error(err);
-        res.status(500).send('Stranica je trenutno nedostupna.');
+        renderErrorPage(res, 'Stranica je trenutno nedostupna.', 500, err.stack);
     }
 });
 
