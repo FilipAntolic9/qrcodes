e5bf7475d17d2a3cbdbda8cb2cb6398ede236a9d
FilipAntolic9 <fantolic@gmail.com>
2024-10-28 01:13:10 +0100

    t12

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ nconf.env()
 
 //oauth - https://manage.auth0.com/ - kreiran Applications / API (M2M)
 const jwtCheck = auth({
-  // audience: 'https://qr-codes-mk80.onrender.com',
+  audience: 'https://qr-codes-mk80.onrender.com',
   issuerBaseURL: 'https://dev-4lu668zsbke41q0u.us.auth0.com/',  //BASIC/ DOMAIN
   tokenSigningAlg: 'RS256'
 });
